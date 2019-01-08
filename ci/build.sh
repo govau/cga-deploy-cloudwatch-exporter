edcc9015f8144507769e1ad09d03c05e0e919be2
Brendan Gordon <brendan.gordon@digital.gov.au>
2019-01-08 17:32:18 +1100

    Pin dep

--- ci/build.sh
+++ ci/build.sh
@@ -8,14 +8,17 @@ ORIG_PWD="${PWD}"
 # Create our own GOPATH
 export GOPATH="${ORIG_PWD}/go"
 
-# Symlink our source dir from inside of our own GOPATH
+# We are using a fork of github.com/technofy/cloudwatch_exporter.
+# Symlink our source dir from inside of our own GOPATH to make sure we build 
+# our fork and not upstream.
 mkdir -p "${GOPATH}/src/github.com/technofy"
 ln -s "${ORIG_PWD}/src" "${GOPATH}/src/github.com/technofy/cloudwatch_exporter"
 cd "${GOPATH}/src/github.com/technofy/cloudwatch_exporter"
 
-go get github.com/golang/dep/cmd/dep
+curl -L https://github.com/golang/dep/releases/download/v0.5.0/dep-linux-amd64 > /usr/local/bin/dep
+chmod a+x /usr/local/bin/dep
 
-go dep ensure
+dep ensure
 
 go build
 
