882c3175cbbfcdfa03e2718f8488740a82f8d8a3
Brendan Gordon <brendan.gordon@digital.gov.au>
2019-03-13 15:54:11 +1100

    Work regardless of where its called from

--- ci/create-pipeline.sh
+++ ci/create-pipeline.sh
@@ -5,12 +5,14 @@ set -eux
 TARGET=${TARGET:-mcld}
 PIPELINE=cloudwatch-exporter
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 export https_proxy=socks5://localhost:8112
 
-fly validate-pipeline --config pipeline.yml
+fly validate-pipeline --config ${SCRIPT_DIR}/pipeline.yml
 
 fly -t ${TARGET} set-pipeline -n \
-  --config pipeline.yml \
+  --config ${SCRIPT_DIR}/pipeline.yml \
   --pipeline "${PIPELINE}"
 
 # Check all resources for errors
